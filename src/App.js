5b361380866e0bbe2327860a81a81be8b9a630ab
chinmayj24 <chinmayjanwalkar24@gmail.com>
2021-04-09 14:31:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import LoginForm from './components/LoginForm';
+import ChatFeed from './components/ChatFeed';
+import './App.css';
+
+const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />
+
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="9737f66d-d803-4b69-8a23-789c04e13e5d"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        />
+    )
+}
+
+export default App;
---
+++ src/components/MyMessage.jsx
@@ -0,0 +1,20 @@
+const MyMessage = ({ message }) => {
+    if (message?.attachments?.length > 0) {  //if it is an image
+        return (
+            <img
+                src={message.attachments[0].file}
+                alt="message-attachment"
+                className="message-image"
+                style={{ float: 'right' }}
+            />
+        )
+    }
+
+    return ( //rendering the text
+        <div className="message" style={{ float: "right", marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
+            {message.text}
+        </div>
+    )
+}
+
+export default MyMessage;
